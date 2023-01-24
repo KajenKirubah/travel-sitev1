49721ffffac50642ea697d8e7ff3ddcba8575b21
Kajen Kirubah <kajenk22@hotmail.com>
2023-01-24 15:56:16 -0500

    Completed the reveal on scroll behavior.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,11 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 if(module.hot) {
     module.hot.accept();
 }
 
 let mobileMenu = new MobileMenu();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
