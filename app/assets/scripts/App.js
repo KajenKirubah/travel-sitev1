345d6bc32180154cdf024797eaae252fe5ff3277
Kajen Kirubah <kajenk22@hotmail.com>
2023-01-26 12:37:58 -0500

    Completed the sticky header functionality.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,13 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 if(module.hot) {
     module.hot.accept();
 }
 
 let mobileMenu = new MobileMenu();
+let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
