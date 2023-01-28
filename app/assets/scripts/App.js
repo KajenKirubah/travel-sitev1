dd4b3e7ae2ce8c107143210cfbf035707a52020b
Kajen Kirubah <kajenk22@hotmail.com>
2023-01-28 14:35:34 -0500

    Completed the modal overlay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,30 @@
-import '../styles/styles.css';
-import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
-import StickyHeader from './modules/StickyHeader';
+import "../styles/styles.css";
+import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
-if(module.hot) {
-    module.hot.accept();
-}
-
-let mobileMenu = new MobileMenu();
-let stickyHeader = new StickyHeader();
+new MobileMenu();
+new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+let modal;
+
+document.querySelectorAll(".open-modal").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    if(typeof modal == "undefined") {
+        import(/*webpackChunkName: "modal" */ "./modules/Modal")
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch((err) => console.log("there was a problem" + err));
+    } else {
+       modal.openTheModal();
+    }
+  });
+});
+
+if (module.hot) {
+  module.hot.accept();
+}
