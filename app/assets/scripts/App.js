f9f9ccd10e69ae6d1dc1d9dd68c8177abe7b0ffd
Kajen Kirubah <kajenk22@hotmail.com>
2023-01-31 20:11:02 -0500

    Added client area feature.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,7 +3,9 @@ import 'lazysizes';
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import ClientArea from "./modules/ClientArea";
 
+new ClientArea();
 new MobileMenu();
 new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
